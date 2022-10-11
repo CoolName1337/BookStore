9e4aeb74dac7355157f16c0f76e962f14ade2e7f
CoolName1337 <81pudola@inbox.ru>
2022-10-11 18:34:19 +0300

    Добавьте файлы проекта.

---
+++ Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models;
+
+public enum Genres
+{
+    Porn,
+    WomansShit,
+    MansShit,
+    Science,
+}
+
+internal class Rating
+{
+    public decimal Get() => stars.Values.Sum() / stars.Count();
+    Dictionary<int, int> stars = new();
+    public void AddStar(int id, int s) => stars.Add(id, s);
+    public void RemoveStar(int id) => stars.Remove(id);
+}
+
+public class Book
+{
+    [Range(0, 1000)]
+    public decimal Price { get; set; }
+    [Required]
+    public string Name { get; set; } = "Undefined";
+    [Required]
+    public string Data { get; set; }
+    public string Description { get; set; }
+
+    private bool isBought = false;
+
+    [Range(0, 5)]
+    private decimal Rating;
+}
---
+++ Pages/BookPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages
+{
+    public class BookPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
