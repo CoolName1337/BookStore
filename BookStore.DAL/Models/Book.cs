68f3f6b0a418d3a477c606b652f2410e81cc9124
CoolName1337 <90530268+CoolName1337@users.noreply.github.com>
2022-10-26 21:02:28 +0300

    Add files via upload

---
+++ BookStore.DAL/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿namespace BookStore.DAL.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Description { get; set; } = "";
+    public string SourceImage { get; set; } = "";
+    public string SourceFile { get; set; } = "";
+    public string Writer { get; set; } = "";
+    public DateTime AddingDate { get; set; }
+    public DateTime DateOfCreation { get; set; }
+    public List<User> Users { get; set; } = new List<User>();
+    public string _ratingString { get; set; } = "";
+
+
+
+    //public void AddRating(int rating)
+    //{
+    //    _ratingString += rating.ToString();
+    //}
+    //public void DeleteRating(int rating)
+    //{
+    //    var list = _ratingString.ToList();
+    //    list.Remove(rating.ToString()[0]);
+    //    _ratingString = string.Join("", list);
+    //}
+
+    //[NotMapped]
+    //public double Rating
+    //{
+    //    get => _ratingString.Length == 0 ? 0 : Math.Round(_ratingString.ToArray().Average(str => int.Parse(str.ToString())), 1, MidpointRounding.ToEven);
+    //}
+    //[NotMapped]
+    //public int RatingCount
+    //{
+    //    get => _ratingString.Length;
+    //}
+
+}
---
+++ BookStore.DAL/Repositories/RepositoryUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BookStore.DAL.Models;
+
+namespace BookStore.DAL.Repositories;
+
+public class RepositoryUser
+{
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+
+    public RepositoryUser(UserManager<User> userManager, SignInManager<User> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<SignInResult> Login(string username, string password, bool remember)
+    {
+        return await _signInManager.PasswordSignInAsync(username, password, remember, false);
+    }
+    public async void Logout()
+    {
+        await _signInManager.SignOutAsync();
+    }
+    public async Task<IdentityResult> Create(User user, string password)
+    {
+        var result = await _userManager.CreateAsync(user, password);
+        return result;
+    }
+
+    public async void Delete(User user)
+    {
+        await _userManager.DeleteAsync(user);
+    }
+
+    public async void Update(User user)
+    {
+        await _userManager.UpdateAsync(user);
+    }
+
+    public User this[string Id]
+    {
+        get => _userManager.FindByIdAsync(Id).Result;
+    }
+}
