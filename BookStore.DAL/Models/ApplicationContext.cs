04743c20bf0c73b25eda5bd34f594a3016c60afb
CoolName1337 <90530268+CoolName1337@users.noreply.github.com>
2022-10-27 14:32:31 +0300

    Cant't merge three projects on github((...
    I don't know how to merge three projects to one github repository((

---
+++ BookStore.DAL/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder opts)
+        {
+            opts.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=usersstoredb;Trusted_Connection=True;");
+        }
+
+    }
+}
