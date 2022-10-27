df174a0eb416d07cf2c5612e40d8e57bbc6adf51
CoolName1337 <90530268+CoolName1337@users.noreply.github.com>
2022-10-27 15:02:20 +0300

    Add files via upload

---
+++ BookStore.DAL/Services/ServiceRole.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.DAL.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.BAL.Services;
+
+public class ServiceRole
+{
+    private RepositoryRole _repositoryRole;
+    public ServiceRole(RoleManager<IdentityRole> roleManager)
+    {
+        _repositoryRole = new(roleManager);
+    }
+
+    public async Task Create(string roleName)
+    {
+        await _repositoryRole.Create(new IdentityRole(roleName));
+    }
+}
