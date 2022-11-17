83525650c2785f8b294620babcac65f04125c690
CoolName1337 <81pudola@inbox.ru>
2022-11-17 19:30:02 +0300

    Update profile page. Work with partial view page.

--- BookStore.BAL/Services/ServiceFavorite.cs
+++ BookStore.BAL/Services/ServiceFavorite.cs
@@ -28,6 +28,5 @@ public class ServiceFavorite : IServiceFavorite
             user.Favorites.Remove(favorite);
             return false;
         }
-        
     }
 }
--- BookStore/Pages/Account/ProfilePage.cshtml.cs
+++ BookStore/Pages/Account/ProfilePage.cshtml.cs
@@ -1,12 +1,54 @@
+using BookStore.BAL.Interfaces;
+using BookStore.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BookStore.Pages.Account
+namespace BookStore.Pages.Account;
+
+public class ProfilePageModel : PageModel
 {
-    public class ProfilePageModel : PageModel
+    public IServiceUser _serviceUser { get; init; }
+    public IServiceBook _serviceBook { get; init; }
+    public IServiceRating _serviceRating { get; init; }
+    public User ProfileUser { get; init; }
+    public List<Book> AvailableBooks { get; init; } = new();
+    public List<Book> FavoriteBooks { get; init; } = new();
+    public ProfilePageModel(IHttpContextAccessor httpContextAccessor, IServiceUser serviceUser, 
+        IServiceBook serviceBook, IServiceRating serviceRating)
     {
-        public void OnGet()
+        _serviceUser = serviceUser;
+        _serviceBook = serviceBook;
+        _serviceRating = serviceRating;
+
+        ProfileUser = _serviceUser.GetUser(httpContextAccessor.HttpContext.User);
+        AvailableBooks = ProfileUser.AvailableBooks;
+
+        foreach (var bookId in ProfileUser.Favorites.Select(favBook => favBook.BookId)) FavoriteBooks.Add(_serviceBook[bookId]);
+        FavoriteBooks.Remove(null);
+    }
+
+    public IActionResult OnGetMyBooks()
+    {
+        return new PartialViewResult()
         {
-        }
+            ViewName = "_MyBooks",
+            ViewData = this.ViewData
+        };
+    }
+    public IActionResult OnGetMyFavorites()
+    {
+        return new PartialViewResult()
+        {
+            ViewName = "_MyFavorites",
+            ViewData = this.ViewData
+        };
+    }
+    public IActionResult OnGetMyReviews()
+    {
+        return new PartialViewResult()
+        {
+            ViewName = "_MyReviews",
+            ViewData = this.ViewData
+        };
     }
 }
